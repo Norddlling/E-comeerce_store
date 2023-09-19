37a65ee44960ecd4f62606ea77b95904a5f6f32f
Norddling <capral96@gmail.com>
2023-09-19 18:11:49 +0300

    create seeder and user/dmin data

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Daddy Admin', 
+                'email' => 'capral96@gmail.com', 
+                'password' => Hash::make('SuperSecurePassword'), 
+                'role' => 'admin',
+                'status' => 'active', 
+            ],
+
+            [
+                'name' => 'First User', 
+                'email' => 'ihorpoberezhnii@gmail.com', 
+                'password' => Hash::make('SecurePassword'), 
+                'role' => 'user',
+                'status' => 'active', 
+            ],
+        ]);
+    }
+}
